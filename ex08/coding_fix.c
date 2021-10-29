fb75e11bfc97606ef9f9d0e9864ccaa3d80200ce
mpivet-p <mpivet-p@student.42.fr>
2021-10-29 19:37:48 +0200

    Reverse done

--- ex08/coding_fix.c
+++ ex08/coding_fix.c
@@ -7,19 +7,21 @@
 #include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
+MODULE_VERSION("1.1");
 MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
 MODULE_DESCRIPTION("Useless module");
 
 static ssize_t myfd_read(struct file *fp, char __user *user,
-		size_t size, loff_t *offs);
+			 size_t size, loff_t *offs);
 static ssize_t myfd_write(struct file *fp, const char __user *user,
-		size_t size, loff_t *offs);
+			  size_t size, loff_t *offs);
 
-static struct file_operations myfd_fops = {
+static const struct file_operations myfd_fops = {
 	.owner = THIS_MODULE,
 	.read = &myfd_read,
 	.write = &myfd_write
 };
+
 static struct miscdevice myfd_device = {
 	.minor = MISC_DYNAMIC_MINOR,
 	.name = "reverse",
@@ -40,29 +42,36 @@ static int __init myfd_init(void)
 
 static void __exit myfd_cleanup(void)
 {
-
+	misc_deregister(&myfd_device);
 }
 
 ssize_t myfd_read(struct file *fp, char __user *user
 		, size_t size, loff_t *offs)
 {
-	size_t len;
-
-	len = strlen(str);
-	return simple_read_from_buffer(user, size, offs, str, len);
+	return simple_read_from_buffer(user, size, offs, str, strlen(str));
 }
 
 ssize_t myfd_write(struct file *fp, const char __user *user
 		, size_t size, loff_t *offs)
 {
+	char *tmp;
 	ssize_t res;
+	size_t i = 0;
+	ssize_t t;
 
-	res = simple_write_to_buffer(str, PAGE_SIZE, offs, user, PAGE_SIZE) + 1;
-	/*
-	 * 0x0 = '\0'
-	 */
-	str[res] = 0x0;
-	return res;
+	tmp = kmalloc(sizeof(char) * PAGE_SIZE + 1, GFP_KERNEL);
+	if (!tmp)
+		return (-ENOMEM);
+	res = simple_write_to_buffer(tmp, PAGE_SIZE, offs, user, size);
+	if (res < 0)
+		return (res);
+	res--;
+	tmp[res] = 0;
+	for (t = res - 1; t >= 0; t--, i++)
+		str[i] = tmp[t];
+	str[i] = '\n';
+	str[i + 1] = 0;
+	return size;
 }
 
 module_init(myfd_init);
